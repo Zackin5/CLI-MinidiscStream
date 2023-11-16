c6fa5cd475ccc87f05b14709c3900f134ea8e73d
Zackin5 <zachin55@gmail.com>
2023-11-15 18:01:18 -0600

    Input choice logic

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+
+use std::io;
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, Sink, cpal::{self, traits::HostTrait, traits::DeviceTrait}};
+
+// fn get_devices() -> rodio::Device {
+fn get_devices() {
+    // List available devices for output
+    println!("Available devices:");
+    let devices:Vec<rodio::Device> = cpal::default_host().output_devices().unwrap().collect();
+
+    for (i, device) in devices.iter().enumerate() {
+        println!(" {}: {}", i, device.name().unwrap());
+    }
+
+    // Get user choice
+    println!("Input choice:");
+    
+    loop {
+        let mut choice_input = String::new();
+        io::stdin().read_line(&mut choice_input).expect("Please provide an input");
+
+        match choice_input.trim().parse::<usize>() {
+            Ok(i) => {
+                // Return result
+                match devices.get(i) {
+                    Some(_) => {
+                        return;
+                    },
+                    None => {
+                        println!("Invalid device index");
+                        continue;
+                    },
+                };
+            },
+
+            Err(..) => {
+                println!("Invalid input");
+                continue;
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("Starting...");
+
+    let output_device = get_devices();
+
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open("D:\\zachi\\Music\\Deftones - Around The Fur\\11 - Deftones - Bong Hit.mp3").unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+
+    // sink.append(source);
+    // sink.sleep_until_end();
+
+    println!("Done!");
+}
